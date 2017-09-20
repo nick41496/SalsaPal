cff16267d197cb02368e79761b51df9fe34deda7
Nicholas White <n6white@uwaterloo.ca>
2017-09-20 19:34:50 -0400

    Initial commit

---
+++ app/src/main/java/co/nickwhite/salsapal/Preferences.java
@@ -0,0 +1,13 @@
+package co.nickwhite.salsapal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Preferences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+}
